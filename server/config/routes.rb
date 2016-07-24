670d7747ca83c2c98411819d3109979283e6377f
mattwesthoff <matt@enlearn.org>
2016-07-23 22:44:49 -0700

    testing link, also fixing route

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'games/index'
-  get '/', to: 'static_pages#home'
+  get '/', to: 'static_pages#home', as: 'home'
   get 'static_pages/help'
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
