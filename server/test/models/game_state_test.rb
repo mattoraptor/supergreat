3799670e36203ceec8810b27c9c4bf008b7de558
mattwesthoff <matt@enlearn.org>
2016-07-25 12:16:02 -0700

    setup

--- server/test/models/game_state_test.rb
+++ server/test/models/game_state_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class GameStateTest < ActiveSupport::TestCase
-  test 'new game state has four cards in each players spot' do
-    test_obj = GameState.new
-    assert_equal(test_obj.hands.length, 4)
+  def setup
+    @test_obj = GameState.new
+  end
+
+  test 'new game state has four hands' do
+    assert_equal(@test_obj.hands.length, 4)
   end
 end
